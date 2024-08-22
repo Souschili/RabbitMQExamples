dee6141b561dc6ef5e479fcb62215e2b43b81946
Orkhan <aliyevorhan82@gmail.com>
2024-08-22 05:39:20 +0400

    add new solution to read message from queue

---
+++ ConsumerRabbitApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsumerRabbitApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
